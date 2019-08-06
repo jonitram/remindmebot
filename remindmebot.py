f4688c78bf06181a99d2a57eab9a12368ee178a6
jonitram <jon.l.nitram@gmail.com>
2019-08-05 22:06:59 -0500

    working on stripping whitespace

--- remindmebot.py
+++ remindmebot.py
@@ -2,6 +2,7 @@
 import asyncio
 import discord
 from dateparser.search import search_dates
+import dateparser
 import datetime
 import re
 
@@ -15,7 +16,10 @@ tokensfile = "tokens.txt"
 # index [0] is the default
 command_prefixes = ['rm','rm!','remindme!','!rm','!remindme']
 
-reminders = {}
+reminder_tasks = {}
+user_reminders = {}
+
+commands = ['help','reminders','delete','deleteall']
 
 # the discord client
 help_activity = discord.Activity(name='\"' + command_prefixes[0] + ' help\" for help',type=discord.ActivityType.playing)
@@ -32,38 +36,55 @@ def setup_tokens(filename):
 
 @client.event
 async def on_message(message):
-    global reminders
     for prefix in command_prefixes:
         if message.content.lower().startswith(prefix + ' '):
-            # remove prefix
-            # check for help / delete / other commands
-            # assume its reminder otherwise
-            asyncio.create_task(create_reminder(message))
-            # async create task send reminder
+            parameters = message.content.replace(prefix, '', 1).lower().split()
+            if len(parameters) == 0:
+                if parameters[0] == 'help':
+                    # help message
+                    return
+                elif parameters[0] == 'reminders':
+                    # display all reminders for that user
+                    return
+                elif parameters[0] == 'deleteall':
+                    # delete all reminders
+                    return
+            elif parameters[0] == 'delete':
+                # delete reminder
+                return
+            else:
+                # assume its reminder otherwise
+                asyncio.create_task(create_reminder(message))
+                # async create task send confirmation
             return
-        # elif auther == bot begins with help
-        #     spam reactions
+    # if auther == bot and begins with confirmation message
+    # attatch reactions
     return
 
 async def create_reminder(message):
-    global reminders
-    reminder_messages = []
-    reminder_times = []
+    global reminder_tasks, user_reminders
     extracted_times = search_dates(message.content, settings={'PREFER_DATES_FROM' : 'future', 'PREFER_DAY_OF_MONTH' : 'first', 'RELATIVE_BASE' : message.created_at})
+    delimiters = command_prefixes
     for i in range(len(extracted_times)):
+        delimiters.append(extracted_times[i][0])
+    regex_pattern = '|'.join(map(re.escape, delimiters))
+    reminder_messages = re.split(regex_pattern, message.content)
+    reminder_messages = [reminder_message.strip() for reminder_message in reminder_messages if reminder_message != '']
+    print(reminder_messages)
+    # for i in range(len(extracted_times)):
         
-        reminder_times.append(extracted_times[i][1])
-    for i in range(len(reminder_times)):
-        # create reminder for each thing
-        if message.id not in reminders.keys():
-            reminders.update({message.id : []})
-        temp = reminders.get(message.id)
-        if i in range(len(reminder_messages)):
-            temp.append(asyncio.create_task(run_reminder(message, reminder_times[i][1], reminder_messages[i])))
-        else:
-            temp.append(asyncio.create_task(run_reminder(message, reminder_times[i][1], '')))
-        reminders.update({message.id : temp})
-        return
+    #     reminder_times.append(extracted_times[i][1])
+    # for i in range(len(reminder_times)):
+    #     # create reminder for each thing
+    #     if message.id not in reminders.keys():
+    #         reminders.update({message.id : []})
+    #     temp = reminders.get(message.id)
+    #     if i in range(len(reminder_messages)):
+    #         temp.append(asyncio.create_task(run_reminder(message, reminder_times[i][1], reminder_messages[i])))
+    #     else:
+    #         temp.append(asyncio.create_task(run_reminder(message, reminder_times[i][1], '')))
+    #     reminders.update({message.id : temp})
+    #     return
 
 async def run_reminder(message, reminder_time, reminder_message):
     delay = reminder_time - datetime.datetime.now()
