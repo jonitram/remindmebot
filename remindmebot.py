39d70d0a747c600b18ec8891a5d642266360961c
Jon Martin <jonnitram@JonNitram.local>
2019-08-08 09:33:19 -0500

    fixed problem with eq in reminder object

--- remindmebot.py
+++ remindmebot.py
@@ -26,7 +26,7 @@ help_messages = [' \"clear\" : Deletes commands issued to the bot and messages s
                  ' \"reminders\" : Sends a list of your active reminders.',
                  ' \"restart\" : Restarts and updates the bot.']
 
-confirmation_options = ['Delete this reminder.',
+confirmation_options = ['Delete this reminder (Will also delete both messages).',
                         'Delete the message that created this reminder.',
                         'Delete this message.',
                         'Delete both messages.']
@@ -63,7 +63,7 @@ class Reminder:
         return hash((self.message, self.time, self.info))
     
     def __eq__(self, other):
-        return (self.message, self.time, self.info) == (self.message, self.time, self.info)
+        return (self.message, self.time, self.info) == (other.message, other.time, other.info)
     
     def __ne__(self, other):
         return not(self == other)
@@ -79,11 +79,12 @@ async def on_message(message):
             parameters = message.content.lower().replace(prefix,'',1).split()
             if len(parameters) == 1:
                 if parameters[0] == 'help':
+                    # done
                     help_message = message.author.mention + build_help_message()
                     asyncio.create_task(message.channel.send(help_message))
                     return
                 elif parameters[0] == 'reminders':
-                    asyncio.create_task(message.channel.send(list_reminders(message.author)))
+                    asyncio.create_task(message.channel.send(print_reminders(message.author)))
                     return
                 elif parameters[0] == 'clear':
                     # clear
@@ -100,7 +101,7 @@ async def on_message(message):
                     return
                 reminder = filter_reminders(message.author, prefix, message.content)
                 if reminder != None:
-                    asyncio.create_task(delete_reminder(reminder))
+                    asyncio.create_task(cancel_reminder(reminder))
                 else:
                     asyncio.create_task(create_reminders(message))
                 return
@@ -117,6 +118,24 @@ async def on_message(message):
                 asyncio.create_task(message.add_reaction(emojis[i]))
     return
 
+async def cancel_reminder(reminder):
+    global user_reminders, reminder_tasks
+    user_reminders[reminder.user].remove(reminder)
+    reminder_tasks[reminder].cancel()
+    del reminder_tasks[reminder]
+    try:
+        await reminder.message.delete()
+    except discord.NotFound:
+        pass
+    try:
+        await reminder.confirmation.delete()
+    except discord.NotFound:
+        pass
+    formatted_time = reminder.time.strftime("%H:%M:%S on %b %d, %Y")
+    result = reminder.user.mention + ' The reminder for \"{0}\" set to go off at {1} has been deleted.'.format(reminder.info,formatted_time)
+    asyncio.create_task(reminder.channel.send(result))
+    return
+
 def filter_reminders(user, prefix, message_content):
     trimmed_content = message_content.replace('delete','')[message_content.index(' '):].strip()
     if user in user_reminders:
@@ -128,45 +147,25 @@ def filter_reminders(user, prefix, message_content):
                 return reminder
     return None
 
-#make this compatible using invalid state erorr like clear from sbb
-async def delete_reminder(reminder):
-    global user_reminders, reminder_tasks
-    if reminder in user_reminders[reminder.user]:
-        user_reminders[reminder.user].remove(reminder)
-    
-    if reminder in reminder_tasks:
-        try:
-            reminder_tasks[reminder].result()
-        except asyncio.base_futures.InvalidStateError:
-            reminder_tasks[reminder].cancel()
-            try:
-                await reminder.message.delete()
-            except discord.NotFound:
-                pass
-            formatted_time = reminder.time.strftime("%H:%M:%S on %b %d, %Y")
-            result = reminder.user.mention + ' The reminder for \"{0}\" set to go off at {1} has been deleted.'.format(reminder.info,formatted_time)
-        else:
-            original_timestamp = reminder.message.created_at.strftime("%H:%M:%S on %b %d, %Y")
-            result = '{0} Reminder for \"{1}\" from {2}.\nHere is a link to the original message: {3}'.format(reminder.user.mention, reminder.info, original_timestamp,reminder.message.jump_url) 
-        del reminder_tasks[reminder]
-    try:
-        await reminder.confirmation.delete()
-    except discord.NotFound:
-        pass
-    asyncio.create_task(reminder.channel.send(result))
-    return
-
 def list_reminders(user):
-    result = None
+    result = []
+    print(user_reminders)
     if user in user_reminders:
-        if len(user_reminders[user]) > 0:
-            result = user.mention + ' Here is a list of your active reminders:'
-            for i in range(len(user_reminders[user])):
-                formatted_time = user_reminders[user][i].time.strftime("%H:%M:%S on %b %d, %Y")
-                result += '\n{0} - \"{1}\" for {2}'.format(1+i,user_reminders[user][i].info,formatted_time)
-    if result == None:
-        result = user.mention + ' You currently have no reminders set!'
+        for reminder in user_reminders[user]:
+            result.append(reminder)
     return result
+
+def print_reminders(user):
+    result = user.mention + ' Here is a list of your active reminders:'
+    index = 1
+    for reminder in list_reminders(user):
+        formatted_time = reminder.time.strftime("%H:%M:%S on %b %d, %Y")
+        result += '\n{0} - \"{1}\" for {2}'.format(index, reminder.info, formatted_time)
+        index += 1
+    if index == 1:
+        result += '\nYou have no active reminders!'
+    return result
+
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -178,36 +177,29 @@ async def on_raw_reaction_add(payload):
             user = client.get_user(payload.user_id)
             # confirmation options
             if message.content.endswith(build_reaction_options(confirmation_options)):
-                if user in user_reminders:
-                    # SITUATION WHERE REMINDER HAS COMPLETED BUT USER STILL INTERACTS WITH MESSAGES
-                    # SHOULD VALIDATE ID BASED ON MENTION AT THE BEGINNING OF MESSAGE THROUGH THE FIRST SPACE
-                    for i in range(len(user_reminders[user])):
-                        if user_reminders[user][i].confirmation.id == message.id:
-                            if option == 0:
-                                asyncio.create_task(delete_reminder(user_reminders[user][i]))
+                if message.content[:message.content.find(' ')].replace('!','') == user.mention:
+                    # extract trigger message
+                    if option == 0:
+                        for reminder in user_reminders[user]:
+                            if reminder.confirmation.content == message.content:
+                                asyncio.create_task(cancel_reminder(reminder))
                                 return
-                            elif option == 1:
-                                try:
-                                    await user_reminders[user][i].message.delete()
-                                except discord.NotFound:
-                                    pass
-                                return
-                            elif option == 2:
-                                try:
-                                    await user_reminders[user][i].confirmation.delete()
-                                except discord.NotFound:
-                                    pass
-                                return
-                            elif option == 3:
-                                try:
-                                    await user_reminders[user][i].message.delete()
-                                except discord.NotFound:
-                                    pass
-                                try:
-                                    await user_reminders[user][i].confirmation.delete()
-                                except discord.NotFound:
-                                    pass
-                                return
+                    if option == 1:
+                        # try delete trigger message
+                        return
+                    elif option == 2:
+                        try:
+                            await message.delete()
+                        except discord.NotFound:
+                            pass
+                        return
+                    elif option == 3:
+                        # try delete trigger message
+                        try:
+                            await message.delete()
+                        except discord.NotFound:
+                            pass
+                        return
             elif message.content.endswith(build_help_message()):
                 help_message = user.mention + help_messages[emojis.index(payload.emoji.name)]
                 asyncio.create_task(channel.send(help_message))
@@ -265,35 +257,47 @@ async def create_reminders(message):
     for i in range(max(len(extracted_times),len(reminder_messages))):
         new_reminder = Reminder(message.author,message,message.channel)
         if i in range(len(extracted_times)):
+            if extracted_times[i][1] < datetime.datetime.now():
+                # error about making a reminder set to go off in the past
+                # print date it would be created at in error
+                return
             new_reminder.time = extracted_times[i][1]
         if i in range(len(reminder_messages)):
             new_reminder.info = reminder_messages[i]
         if message.author not in user_reminders:
             user_reminders[message.author] = []
-        user_reminders[message.author].append(new_reminder)
-        # create task for sleeping and append to remind_tasks
-        reminder_tasks[new_reminder] = asyncio.create_task(run_reminder(new_reminder))
         formatted_time = new_reminder.time.strftime("%H:%M:%S on %b %d, %Y")
         confirmation_message = '{0} A reminder has been created for \"{1}\" and has been set to go off at {2}.\nReact to this message with these reactions to perform these commands:\n{3}'.format(new_reminder.user.mention, new_reminder.info, formatted_time, build_reaction_options(confirmation_options))
         new_reminder.confirmation = await new_reminder.channel.send(confirmation_message)
+        user_reminders[message.author].append(new_reminder)
+        # create task for sleeping and append to remind_tasks
+        reminder_tasks[new_reminder] = asyncio.create_task(run_reminder(new_reminder))
     
     # debugging
-    for key in user_reminders.keys():
-        for i in range(len(user_reminders[key])):
-            user_reminders[key][i].to_string()
+    # for key in user_reminders.keys():
+    #     for i in range(len(user_reminders[key])):
+    #         user_reminders[key][i].to_string()
+
+    return
 
 async def run_reminder(reminder):
-    global user_reminders, reminder_tasks
-
     # set delay once but run into the problem of negative numbers for the past
     # delay = reminder.time - datetime.datetime.now()
     # await asyncio.sleep(delay.total_seconds())
     # await reminder.channel.send(reminder.info)
-
-    #check constantly with micro sleeps
+    original_timestamp = reminder.message.created_at.strftime("%H:%M:%S on %b %d, %Y")
+    result = '{0} Reminder for \"{1}\" from {2}.\nHere is a link to the original message: {3}'.format(reminder.user.mention, reminder.info, original_timestamp,reminder.message.jump_url) 
+    
+    # check constantly with micro sleeps
     while datetime.datetime.now() < reminder.time:
         await asyncio.sleep(0.1)
-    await delete_reminder(reminder)
+    
+    # send reminder
+    await reminder.channel.send(result)
+
+    # clear global lists
+    user_reminders[reminder.user].remove(reminder)
+    del reminder_tasks[reminder]
     return
 
 # main function
